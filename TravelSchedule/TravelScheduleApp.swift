168157d46e2055fbdd917d0662cf36fe81f11663
nsanikitin <nsa010995@gmail.com>
2024-09-19 12:52:45 +0300

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Сергей Никитин on 19.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelScheduleTests/TravelScheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleTests.swift
+//  TravelScheduleTests
+//
+//  Created by Сергей Никитин on 19.09.2024.
+//
+
+import Testing
+@testable import TravelSchedule
+
+struct TravelScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
